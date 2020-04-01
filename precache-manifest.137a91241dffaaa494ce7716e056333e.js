7dc78acaca3e069691a69bfabe3ce6840ba202bb
Ivo Krastev <ivo.krustev@yahoo.com>
2020-04-01 16:34:29 +0300

    Updates

---
+++ precache-manifest.137a91241dffaaa494ce7716e056333e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f38d3de854d8f7107f8f14ccec4df4",
+    "url": "/weather-app-live/index.html"
+  },
+  {
+    "revision": "b686446dcff92a32135c",
+    "url": "/weather-app-live/static/css/main.c48d127e.chunk.css"
+  },
+  {
+    "revision": "c808f099fb1f8badfc9d",
+    "url": "/weather-app-live/static/js/2.d2299993.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app-live/static/js/2.d2299993.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b686446dcff92a32135c",
+    "url": "/weather-app-live/static/js/main.e0ff7a2b.chunk.js"
+  },
+  {
+    "revision": "e4f9228483e691a54e7a",
+    "url": "/weather-app-live/static/js/runtime-main.912e9b7f.js"
+  },
+  {
+    "revision": "f2c1a60563bb19c24e1d81e19034b2cd",
+    "url": "/weather-app-live/static/media/cold.f2c1a605.jpg"
+  },
+  {
+    "revision": "0aece8e81d73efe485b6e241bfbab16f",
+    "url": "/weather-app-live/static/media/drizzle.0aece8e8.png"
+  },
+  {
+    "revision": "d6d5687d51a8ebca6db248624b975460",
+    "url": "/weather-app-live/static/media/hot.d6d5687d.jpg"
+  },
+  {
+    "revision": "0bdeaae6963af1fbaced43780ebce353",
+    "url": "/weather-app-live/static/media/minus.0bdeaae6.jpg"
+  },
+  {
+    "revision": "9e6b27b65cae17678cda63cc17fa5a71",
+    "url": "/weather-app-live/static/media/stormy.9e6b27b6.png"
+  },
+  {
+    "revision": "72cef3f221c6f75fa219c09c458af6f1",
+    "url": "/weather-app-live/static/media/sunny.72cef3f2.png"
+  },
+  {
+    "revision": "6303f2d843982a0d0dff3aca2dc17c93",
+    "url": "/weather-app-live/static/media/thunder.6303f2d8.png"
+  },
+  {
+    "revision": "2f66212ce2fc3be9a691ef547e82353a",
+    "url": "/weather-app-live/static/media/warm.2f66212c.jpg"
+  }
+]);
